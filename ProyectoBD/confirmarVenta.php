b004546f7624d79372386201c8f763601cb6309b
Luisnerk <64331754+Luisnerk@users.noreply.github.com>
2021-12-09 20:18:48 -0600

    Update confirmarVenta.php

--- ProyectoBD/confirmarVenta.php
+++ ProyectoBD/confirmarVenta.php
@@ -72,7 +72,7 @@ incluirTemplate('header');
 <html lang="en">
 <header>
     <div class="regresar">
-        <a href="principal.php">
+        <a href="seleccionarSucursal.php">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
             </svg>
